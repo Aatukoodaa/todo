3cc9fc8dd6df8a8e3c7993d7fd62109aff3e2b83
Aatukoodaa <59015331+Aatukoodaa@users.noreply.github.com>
2019-12-18 13:35:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -41,17 +41,4 @@ $(document).ready(function() {
     localStorage.setItem("listItems", $("#list-items").html());
   });
 });
-<head>
-<script language="JavaScript">
 
-var pass_entered;
-var password="cool";
-
-while (pass_entered!=password) {
-    pass_entered=prompt('Please enter the password:','');
-}
-
-self.close();
-
-</script>
-</head>
