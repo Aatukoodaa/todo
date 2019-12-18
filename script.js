b4399cc32bff8c15fb5ee74982e4373f9a564480
Aatukoodaa <59015331+Aatukoodaa@users.noreply.github.com>
2019-12-18 13:41:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+  function passWord() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter Your Password',' ');
+    while (testV < 3) {
+        if (!pass1) history.go(-1);
+        if (pass1.toLowerCase() == "letmein") {
+            alert('You Got it Right!');
+            window.open('protectpage.html');
+            break;
+        } 
+        testV+=1;
+        var pass1 = prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+    }
+    if (pass1.toLowerCase()!="password" & testV ==3) history.go(-1);
+    return " ";
+}
   $("#list-items").html(localStorage.getItem("listItems"));
 
   $(".add-items").submit(function(event) {
@@ -42,26 +58,7 @@ $(document).ready(function() {
   });
 });
 
-function passWord() {
-    var testV = 1;
-    var pass1 = prompt('Please Enter Your Password',' ');
-    while (testV < 3) {
-        if (!pass1) history.go(-1);
-        if (pass1.toLowerCase() == "letmein") {
-            alert('You Got it Right!');
-            window.open('protectpage.html');
-            break;
-        } 
-        testV+=1;
-        var pass1 = prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
-    }
-    if (pass1.toLowerCase()!="password" & testV ==3) history.go(-1);
-    return " ";
-}
 
-<CENTER>
-<FORM>
-<input type="button" value="Enter Protected Area" onClick="passWord()">
-</FORM>
-</CENTER>
+
+
 
